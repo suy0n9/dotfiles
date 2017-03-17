5bfa134075700c01dffb68825473f6bb4db74721
suy0ng <s.goto823@gmail.com>
2017-03-18 04:29:36 +0900

    Add ansible

---
+++ dotfiles/symlink.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+for f in .??*
+do
+    [[ "$f" == ".git" ]] && continue
+    [[ "$f" == ".DS_Store" ]] && continue
+
+    # check symlink & unlink
+    [[ -L ~/$f ]] && unlink ~/$f
+
+    # check dotfiles & archive
+    if [ -e ~/$f ]; then
+        echo "$f is already exists."
+
+        TIMESTAMP=`date +%Y%m%d%H%M`
+        backupName="${f}_backup_${TIMESTAMP}"
+
+        mv ~/$f ~/$backupName
+        echo "Archived dotfiles ~/$f -> ~/$backupName"
+    fi
+
+    # set symlink
+    filePath="~/mac_setup/dotfiles/${f}"
+    ln -s $filePath ~/$f
+    echo "create symbolic link [ $f -> $filePath ]"
+done
--- setup.sh
+++ setup.sh
@@ -1,9 +1,58 @@
-#!/bin/bash
+#!/bin/sh
 
-for f in .??*
-do
-    [[ "$f" == ".git" ]] && continue
-    [[ "$f" == ".DS_Store" ]] && continue
+echo 'Start setup'
+cd ~
 
-    ln -s ~/dotfiles/$f ~/$f
-done
+echo 'generate keypair?[Y/n]'
+read ANSWER
+case $ANSWER in
+  "" | "Y" | "y" )
+    ssh-keygen ;;
+  * ) echo "generate keypair skip" ;;
+esac
+
+echo 'clone dotfiles?[Y/n]'
+read ANSWER
+case $ANSWER in
+  "" | "Y" | "y" )
+    git clone https://github.com/suy0ng/dotfiles.git ;;
+  * ) echo "clone dotfile skip" ;;
+esac
+
+echo 'install homebrew?[Y/n]'
+read ANSWER
+case $ANSWER in
+  "" | "Y" | "y" )
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" ;;
+  * ) echo "install homebrew skip" ;;
+esac
+
+echo 'install ansible?[Y/n]'
+read ANSWER
+case $ANSWER in
+  "" | "Y" | "y" )
+    brew install python
+    brew install ansible
+    rehash ;;
+  * ) echo "install ansible skip" ;;
+esac
+
+echo 'run ansible?[Y/n]'
+read ANSWER
+case $ANSWER in
+  "" | "Y" | "y" )
+    cd ~/mac_setup
+    ansible-playbook -i hosts -vv localhost.yaml ;;
+  * ) echo "run ansible skip" ;;
+esac
+
+echo 'setup dotfiles?[Y/n]'
+read ANSWER
+case $ANSWER in
+  "" | "Y" | "y" )
+    cd ~/mac_setup/dotfiles
+    ./symlink.sh ;;
+  * ) echo "setup dotfiles skip" ;;
+esac
+
+echo 'new Mac setup finished!! Please run "chsh -s /bin/zsh" & "exec $SHELL"'
