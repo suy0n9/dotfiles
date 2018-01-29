c64a16ec147414e9074b22cdb5d665f250ba0666
suy0n9 <suy01v9@gmail.com>
2018-01-29 21:33:18 +0900

    update symlink.sh

--- symlink.sh
+++ symlink.sh
@@ -4,28 +4,16 @@ for f in .??*
 do
     [[ "$f" == ".git" ]] && continue
     [[ "$f" == ".DS_Store" ]] && continue
-    [[ "$f" == ".swp" ]] && continue
     [[ "$f" == ".gitignore" ]] && continue
 
-    # check symlink & unlink
-    [[ -L ~/$f ]] && unlink ~/$f
-
-    # check dotfiles & archive
-    if [ -e ~/$f ]; then
-        echo "$f is already exists."
-
-        TIMESTAMP=`date +%Y%m%d%H%M`
-        backupName=${f}_backup_${TIMESTAMP}
-
-        mv ~/$f ~/$backupName
-        echo "Archived dotfile ~/$f -> ~/$backupName"
-    fi
+    echo "===$f==="
 
     # set symlink
-    filePath=$HOME/dev/src/github.com/suy0n9/dotfiles/${f}
-    ln -sv $filePath ~/$f
-    echo "create symbolic link [ $f -> $filePath ]"
+    ln -sv $PWD/$f $HOME
 done
 
 # set symlink for flake8
-ln -s ~/dev/src/github.com/suy0n9/dotfiles/flake8 ~/.config/flake8
+FLAKE8PATH=".config/flake8"
+echo "===$FLAKE8PATH==="
+ln -s $PWD/$FLAKE8PATH ~/$FLAKE8PATH
+
