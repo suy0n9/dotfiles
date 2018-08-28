3890a449000e645ed38d059c1c4c3433430207d3
suy0n9 <suy01v9@gmail.com>
2018-08-28 11:04:01 +0900

    fix zsh theme

--- .zsh/zplug.zsh
+++ .zsh/zplug.zsh
@@ -1,5 +1,10 @@
 zplug 'zplug/zplug', hook-build:'zplug --self-manage'
-zplug "themes/candy", from:oh-my-zsh
+
+#zplug "themes/candy", from:oh-my-zsh
+#zplug "dracula/zsh", as:theme
+zplug mafredri/zsh-async, from:github
+zplug sindresorhus/pure, use:pure.zsh, from:github, as:theme
+
 zplug "zsh-users/zsh-completions"
 zplug "zsh-users/zsh-syntax-highlighting"
 zplug "zsh-users/zsh-history-substring-search"
