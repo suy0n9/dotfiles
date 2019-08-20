c907e9a42a8c76657ab82b3d778caeb5981990de
suy0n9 <suy01v9@gmail.com>
2019-08-20 10:41:41 +0900

    remove karabiner settings symlink

--- deploy-config.sh
+++ deploy-config.sh
@@ -6,8 +6,3 @@ if [ ! -f ${HOME}/.config/peco/config.json ]; then
   mkdir -p ${HOME}/.config/peco
   ln -sfv ${BASE}/.config/peco/config.json ${HOME}/.config/peco/config.json
 fi
-
-if [ ! -f ${HOME}/.config/karabiner/karabiner.json ]; then
-  mkdir -p ${HOME}/.config/karabiner
-  ln -sfv ${BASE}/.config/karabiner/karabiner.json ${HOME}/.config/karabiner/karabiner.json
-fi
