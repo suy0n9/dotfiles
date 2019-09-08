af38943fa4a73faa3d51f33d250cfe361e04039b
suy0n9 <suy01v9@gmail.com>
2019-09-08 19:37:25 +0900

    add kube tmux

--- install-plug.sh
+++ install-plug.sh
@@ -2,11 +2,15 @@
 
 # vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # vim color scheme
 curl -fLo ~/.vim/colors/Tomorrow-Night-Bright.vim --create-dirs \
-      https://raw.githubusercontent.com/chriskempson/vim-tomorrow-theme/master/colors/Tomorrow-Night-Bright.vim
+  https://raw.githubusercontent.com/chriskempson/vim-tomorrow-theme/master/colors/Tomorrow-Night-Bright.vim
+
+# kube-tmux
+curl -fLo ~/.tmux/kube.tmux --create-dirs \
+  https://raw.githubusercontent.com/jonmosco/kube-tmux/master/kube.tmux
 
 # tmux plug
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
