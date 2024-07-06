f50bf026dc0e9ef9c5b80de90f1c10b9c7d68c60
suy0n9 <suy01v9@gmail.com>
2024-07-07 00:40:35 +0900

    fix scripts

---
+++ scripts/run_once_install-plug.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# vim-plug
+if [ ! -f "${HOME}/.vim/autoload/plug.vim" ]; then
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+# vim color scheme
+if [ ! -f "${HOME}/.vim/colors/Tomorrow-Night-Bright.vim" ]; then
+  curl -fLo ~/.vim/colors/Tomorrow-Night-Bright.vim --create-dirs \
+    https://raw.githubusercontent.com/chriskempson/vim-tomorrow-theme/master/colors/Tomorrow-Night-Bright.vim
+fi
+# Tmux Plugin Manager
+if [ ! -d "$XDG_DATA_HOME/tmux/plugins/tpm" ]; then
+  git clone https://github.com/tmux-plugins/tpm $XDG_DATA_HOME/tmux/plugins/tpm
+fi
+
