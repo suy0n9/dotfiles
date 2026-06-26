074c0aa1d568b6dcadee9276a51b0820c64929dd
suy0n9 <suy01v9@gmail.com>
2026-06-26 15:07:12 +0900

    fix brew command not found after fresh install

--- scripts/run_once_install-brew.sh
+++ scripts/run_once_install-brew.sh
@@ -5,6 +5,7 @@
 if ! (type brew >/dev/null 2>&1); then
     echo '[INFO] ************ Installing Homebrew'
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 else
     echo '[INFO] ************ Homebrew is already exist'
 fi
