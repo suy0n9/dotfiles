7536e2b1a981ab8594ca191afadcd93a3a40d98b
suy0n9 <suy01v9@gmail.com>
2026-06-26 15:09:01 +0900

    fix brew PATH for both fresh install and existing install

--- scripts/run_once_install-brew.sh
+++ scripts/run_once_install-brew.sh
@@ -5,10 +5,11 @@
 if ! (type brew >/dev/null 2>&1); then
     echo '[INFO] ************ Installing Homebrew'
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    eval "$(/opt/homebrew/bin/brew shellenv)"
 else
     echo '[INFO] ************ Homebrew is already exist'
 fi
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 echo '[INFO] ************ Installing with brew bundle'
 brew bundle --file="$HOME/.Brewfile"
